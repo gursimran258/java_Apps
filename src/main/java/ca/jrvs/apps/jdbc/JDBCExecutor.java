2ca82fce02ed65fff92bac3d1930a02b16d230a4
gursimran <gursimran.gill17@gmail.com>
2019-07-03 11:25:58 -0400

    dao implementation - need to fix orderDAO

--- src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,5 +1,7 @@
 package ca.jrvs.apps.jdbc;
 
+import com.sun.tools.corba.se.idl.constExpr.Or;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -7,27 +9,14 @@ import java.sql.Statement;
 
 public class JDBCExecutor {
     public static void main(String[] args) throws SQLException {
-        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport","postgres", "password");
-        try {
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
+                "hplussport", "postgres", "password");
+        try{
             Connection connection = dcm.getConnection();
-            CustomerDAO customerDAO = new CustomerDAO(connection);
-          Customer customer = new Customer();
-          customer.setFirstName("tom");
-          customer.setLastName("buchka");
-          customer.setEmail("tom@gmail.com");
-          customer.setAddress("123 address");
-          customer.setCity("new jersey");
-          customer.setState("VA");
-          customer.setPhone("(123) 123-1234");
-          customer.setZipCode("12345");
-
-          Customer dbcustomer = customerDAO.create(customer);
-            System.out.println(dbcustomer);
-            dbcustomer = customerDAO.findById(dbcustomer.getId());
-            System.out.println(dbcustomer);
-            dbcustomer.setEmail("john.adam@wh.go");
-
-            } catch (SQLException e) {
+            OrderDAO orderDAO = new OrderDAO(connection);
+            Order order = orderDAO.findById(1000);
+            System.out.println(order);
+        }catch(SQLException e){
             e.printStackTrace();
         }
     }
